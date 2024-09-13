466081f37edcce3a7b9d92e662cf8c9f013c3992
Akshay Kumar <akshay.kumar@system73.com>
2024-09-13 17:32:16 +0530

    make progress in meal app

--- meal/lib/screens/meals_details.dart
+++ meal/lib/screens/meals_details.dart
@@ -7,15 +7,62 @@ class MealsDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(meal.title),
-      ),
-      body: Image.network(
-        meal.imageUrl,
-        height: 300,
-        width: double.infinity,
-        fit: BoxFit.cover,
-      ),
-    );
+        appBar: AppBar(
+          title: Text(meal.title),
+        ),
+        body: Column(
+          children: [
+            Image.network(
+              meal.imageUrl,
+              height: 300,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            const Text(
+              "Ingredients",
+              style: TextStyle(
+                color: Colors.white,
+                decoration: TextDecoration.underline,
+                decorationColor: Colors.white,
+                decorationStyle: TextDecorationStyle.wavy,
+              ),
+            ),
+            for (final ingredient in meal.ingredients)
+              Text(
+                ingredient,
+                style: const TextStyle(
+                  color: Colors.white,
+                  decoration: TextDecoration.underline,
+                  decorationColor: Colors.white,
+                  decorationStyle: TextDecorationStyle.wavy,
+                ),
+              ),
+            const SizedBox(
+              height: 25,
+            ),
+            const Text(
+              "=========Steps=========",
+              style: TextStyle(
+                color: Colors.white,
+                decoration: TextDecoration.underline,
+                decorationColor: Colors.white,
+                decorationStyle: TextDecorationStyle.wavy,
+              ),
+            ),
+            for (final ingredient in meal.steps)
+              Text(
+                ingredient,
+                style: const TextStyle(
+                  color: Colors.white,
+                  decoration: TextDecoration.underline,
+                  decorationColor: Colors.white,
+                  decorationStyle: TextDecorationStyle.wavy,
+                ),
+              )
+          ],
+        ));
   }
 }
